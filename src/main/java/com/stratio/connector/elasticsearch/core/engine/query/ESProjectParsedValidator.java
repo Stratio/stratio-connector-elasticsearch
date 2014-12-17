bd6d89f400f58bdb5274c22e489f8a7a90113d78
josegom <josegom@gmail.com>
2014-12-17 12:56:38 +0100

    CON-20 adapted verify projetParsed to ES

---
+++ src/main/java/com/stratio/connector/elasticsearch/core/engine/query/ESProjectParsedValidator.java
@@ -0,0 +1,9 @@
+package com.stratio.connector.elasticsearch.core.engine.query;
+
+import com.stratio.connector.commons.engine.query.ProjectValidator;
+
+/**
+ * Created by jmgomez on 17/12/14.
+ */
+public class ESProjectParsedValidator implements ProjectValidator {
+}
