3f88db59f6a458129a7418104179e5a593f1b705
luismcl <yo@luismiguelcisneros.com>
2015-06-22 12:12:03 +0200

    Add a new test

--- connector-elasticsearch/src/test/java/com/stratio/connector/elasticsearch/core/engine/utils/SelectCreatorTest.java
+++ connector-elasticsearch/src/test/java/com/stratio/connector/elasticsearch/core/engine/utils/SelectCreatorTest.java
@@ -1,7 +1,43 @@
 package com.stratio.connector.elasticsearch.core.engine.utils;
 
-/**
- * Created by lcisneros on 22/06/15.
- */
+
+import com.stratio.crossdata.common.data.ColumnName;
+import com.stratio.crossdata.common.logicalplan.Select;
+import com.stratio.crossdata.common.statements.structures.ColumnSelector;
+import com.stratio.crossdata.common.statements.structures.Selector;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class SelectCreatorTest {
+
+    @Test
+    public void testModify(){
+        SearchRequestBuilder requestBuilder = mock(SearchRequestBuilder.class);
+        Select select = mock(Select.class);
+        SelectCreator selectCreator = new SelectCreator();
+
+        Map<Selector, String> columnMap = new HashMap<>();
+        when(select.getColumnMap()).thenReturn(columnMap);
+
+        ColumnSelector columnSelector = mock(ColumnSelector.class);
+        ColumnName columnName = mock(ColumnName.class);
+        when(columnSelector.getColumnName()).thenReturn(columnName);
+        when(columnName.getName()).thenReturn("columnName");
+
+        columnMap.put(columnSelector, "columnName");
+
+        //Experimentation
+        selectCreator.modify(requestBuilder, select);
+
+        //Expectations
+        String[] fields = new String[]{"columnName"};
+        Mockito.verify(requestBuilder).addFields(fields);
+    }
 }
